0aa6bc10d6c497f159ea10a8681de550d2f59ba5
Ankit Karn <ankit.karn.ak@gmail.com>
2021-01-23 21:34:52 +0530

    refractor: prettier code

---
+++ README.md
@@ -0,0 +1,4 @@
+# Programming Humor
+
+Bored while coding? have a look at some humor & jokes related with programmers and programming.
+
--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-var refreshButton=document.querySelector('#txt-button');
-var outputDiv=document.querySelector('#output-div');
-var outputDiv2=document.querySelector('#output-div-two');
+var refreshButton = document.querySelector('#txt-button');
+var outputDiv = document.querySelector('#output-div');
+var outputDiv2 = document.querySelector('#output-div-two');
 
 
-var apiURL="https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+var apiURL = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
 
 
-function errorHandler(error){
-    console.log("error occured ",error);
+function errorHandler(error) {
+    console.log("error occured ", error);
     alert("error")
 }
 
-function getJokes(){
+function getJokes() {
     fetch(apiURL)
-    .then(response => response.json())
-    .then(json => {
-        if(json.type==='single'){
-            var outputText=json.joke;
-            outputDiv.innerText= outputText;
-            outputDiv2.innerText="";
-        }else{
-            var outputText=json.setup;
-            outputDiv.innerText= outputText;
-            outputDiv2.innerText=json.delivery;
-        }
-        console.log("joke type: ",json.type);
-        
-    })
-    .catch(errorHandler);
+        .then(response => response.json())
+        .then(json => {
+            if (json.type === 'single') {
+                var outputText = json.joke;
+                outputDiv.innerText = outputText;
+                outputDiv2.innerText = "";
+            } else {
+                var outputText = json.setup;
+                outputDiv.innerText = outputText;
+                outputDiv2.innerText = json.delivery;
+            }
+            console.log("joke type: ", json.type);
+
+        })
+        .catch(errorHandler);
 }
 
 
 
-function clickHandler(){
-    
+function clickHandler() {
+
     getJokes();
 }
 
 
 getJokes();
 
-refreshButton.addEventListener("click",clickHandler)
+refreshButton.addEventListener("click", clickHandler)
