4f6773d146392f7be1f5c3bc6c1394a2916bbae4
Ankit Karn <ankit.karn.ak@gmail.com>
2021-01-23 19:00:05 +0530

    added JS, basic functionality done

---
+++ index.js
@@ -0,0 +1,44 @@
+var refreshButton=document.querySelector('#txt-button');
+var outputDiv=document.querySelector('#output-div');
+var outputDiv2=document.querySelector('#output-div-two');
+
+
+var apiURL="https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+
+
+
+function errorHandler(error){
+    console.log("error occured ",error);
+    alert("error")
+}
+
+
+
+function clickHandler(){
+    
+    fetch(apiURL)
+    .then(response => response.json())
+    .then(json => {
+        if(json.type==='single'){
+            var outputText=json.joke;
+            outputDiv.innerText= outputText;
+            outputDiv2.innerText="";
+        }else{
+            var outputText=json.setup;
+            outputDiv.innerText= outputText;
+            outputDiv2.innerText=json.delivery;
+        }
+        console.log(json.type);
+        
+    })
+    .catch(errorHandler);
+}
+
+
+
+
+
+
+
+
+refreshButton.addEventListener("click",clickHandler)
