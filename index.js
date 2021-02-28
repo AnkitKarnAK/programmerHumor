9f5e0d7dbfe66a79757bd59be77933ee77ecca81
Ankit Karn <ankit.karn.ak@gmail.com>
2021-02-28 22:40:49 +0530

    refactor: async/await, removed outline on focus

--- index.js
+++ index.js
@@ -1,44 +1,59 @@
-var refreshButton = document.querySelector('#txt-button');
-var outputDiv = document.querySelector('#output-div');
-var outputDiv2 = document.querySelector('#output-div-two');
+const refreshButton = document.querySelector('#txt-button');
+const outputDiv = document.querySelector('#output-div');
+const outputDiv2 = document.querySelector('#output-div-two');
 
 
-var apiURL = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+const apiURL = "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
 
 
-function errorHandler(error) {
-    console.log("error occured ", error);
-    alert("error")
+// function errorHandler(error) {
+//     console.log("error occured ", error);
+//     alert("error")
+// }
+
+// function getJokes() {
+//     fetch(apiURL)
+//         .then(response => response.json())
+//         .then(json => {
+//             if (json.type === 'single') {
+//                 const outputText = json.joke;
+//                 outputDiv.innerText = outputText;
+//                 outputDiv2.innerText = "";
+//             } else {
+//                 const outputText = json.setup;
+//                 outputDiv.innerText = outputText;
+//                 outputDiv2.innerText = json.delivery;
+//             }
+//             console.log("joke type: ", json.type);
+
+//         })
+//         .catch(errorHandler);
+// }
+
+const getJokes = async () => {
+    try{
+    const response = await fetch(apiURL);
+    const json = await response.json();
+    if (json.type === 'single') {
+        const outputText = json.joke;
+        outputDiv.innerText = outputText;
+        outputDiv2.innerText = "";
+    } else {
+        const outputText = json.setup;
+        outputDiv.innerText = outputText;
+        outputDiv2.innerText = json.delivery;
+    }
+    console.log("joke type: ", json.type);
+
+    }catch(error){
+        console.log("error occured ", error);
+        alert("error")
+    }
 }
 
-function getJokes() {
-    fetch(apiURL)
-        .then(response => response.json())
-        .then(json => {
-            if (json.type === 'single') {
-                var outputText = json.joke;
-                outputDiv.innerText = outputText;
-                outputDiv2.innerText = "";
-            } else {
-                var outputText = json.setup;
-                outputDiv.innerText = outputText;
-                outputDiv2.innerText = json.delivery;
-            }
-            console.log("joke type: ", json.type);
-
-        })
-        .catch(errorHandler);
-}
 
 
 
-function clickHandler() {
 
-    getJokes();
-}
-
-
-getJokes();
-
-refreshButton.addEventListener("click", clickHandler)
+refreshButton.addEventListener("click", getJokes);
